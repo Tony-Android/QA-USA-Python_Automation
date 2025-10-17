0d2086b30e67972b617bbcb53f5f32e89311e2db
Tony-Android <anthony09141991@gmail.com>
2025-10-17 14:34:32 -0500

    main.py revised commit

--- main.py
+++ main.py
@@ -1,51 +1,60 @@
 import data
 import helpers
+from data import URBAN_ROUTES_URL
+from helpers import is_url_reachable
+
 
 class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
-        if is_url_reachable from helpers.py(URBAN_ROUTES_URL):
-
+        if is_url_reachable(URBAN_ROUTES_URL):
             print("Connected to Urban Route Server")
-            else:
-
+        else:
             print("Cannot connect to Urban Routes. Check the server is on and still running.")
+
 
 def test_set_route(self):
     # Add in S8
     print("function created for set route")
     pass
 
+
 def test_select_plan(self):
     # Add in S8
     print("function created for set route")
     pass
+
 
 def test_fill_phone_number(self):
     # Add in S8
     print("function created for set route")
     pass
 
+
 def test__fill_card(self):
     # Add in S8
     print("function created for set route")
     pass
+
 
 def test_comment_for_driver(self):
     # Add in S8
     print("function created for set route")
     pass
 
+
 def test_order_blanket_and_handkerchiefs(self):
     # Add in S8
     print("function created for set route")
     pass
+
 
 def test_order_2_ice_creams(self):
     for i in range(2):
         # Add in S8
         print("function created for set route")
         pass
+
 
 def test_car_search_model_appears(self):
     # Add in S8
