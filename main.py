ae7e4cc45eef0eb1891320ae031b65bf9c6a2799
Tony-Android <anthony09141991@gmail.com>
2025-10-17 13:38:25 -0500

    TASK 4 commit

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
-    import data
+import data
+import helpers
+
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if is_url_reachable from helpers.py(URBAN_ROUTES_URL):
 
+            print("Connected to Urban Route Server")
+            else:
 
-def test_set_route(self): # Add in S8
-    pass
+            print("Cannot connect to Urban Routes. Check the server is on and still running.")
 
-def test_select_plan(self):# Add in S8
-    pass
-
-def test_fill_phone_number(self):# Add in S8
-    pass
-
-def test__fill_card(self):# Add in S8
+def test_set_route(self):
+    # Add in S8
     print("function created for set route")
     pass
 
-def test_comment_for_driver(self):# Add in S8
+def test_select_plan(self):
+    # Add in S8
     print("function created for set route")
     pass
 
-def test_order_blanket_and_handkerchiefs(self):# Add in S8
+def test_fill_phone_number(self):
+    # Add in S8
     print("function created for set route")
     pass
 
-def test_order_2_ice_creams(self):# Add in S8
+def test__fill_card(self):
+    # Add in S8
     print("function created for set route")
     pass
 
-def test_car_search_model_appears(self):# Add in S8
+def test_comment_for_driver(self):
+    # Add in S8
+    print("function created for set route")
+    pass
+
+def test_order_blanket_and_handkerchiefs(self):
+    # Add in S8
+    print("function created for set route")
+    pass
+
+def test_order_2_ice_creams(self):
+    # Add in S8
+    print("function created for set route")
+    pass
+
+def test_car_search_model_appears(self):
+    # Add in S8
     print("function created for set route")
     pass
