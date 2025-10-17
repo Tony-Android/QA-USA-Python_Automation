a8bc010c19555369faa86e144246cd9bde811f6e
Tony-Android <anthony09141991@gmail.com>
2025-10-17 13:13:59 -0500

    TASK 2 commit

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-
+    import data
 class TestUrbanRoutes:
+
+
+def test_set_route(self): # Add in S8
+    pass
+
+def test_select_plan(self):# Add in S8
+    pass
+
+def test_fill_phone_number(self):# Add in S8
+    pass
+
+def test__fill_card(self):# Add in S8
+    print("function created for set route")
+    pass
+
+def test_comment_for_driver(self):# Add in S8
+    print("function created for set route")
+    pass
+
+def test_order_blanket_and_handkerchiefs(self):# Add in S8
+    print("function created for set route")
+    pass
+
+def test_order_2_ice_creams(self):# Add in S8
+    print("function created for set route")
+    pass
+
+def test_car_search_model_appears(self):# Add in S8
+    print("function created for set route")
+    pass
